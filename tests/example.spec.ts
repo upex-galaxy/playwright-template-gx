ce8c68af9caff3ad889842992f45269a79a86d0d
Ely <elyermad@gmail.com>
2023-02-22 18:05:27 -0300

    new settings and test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -21,11 +21,15 @@ test.describe('UserStory:aquí se escribe el título de la User Story', () => {
 		await todoInput.fill(secondItem)
 		await todoInput.press('Enter')
 
-		const TodoItem = page.locator('[data-testid="todo-item"] .view').getByText(firstItem)
+		const TodoItem = page
+			.getByTestId('todo-item')
+			.locator('.view')
+			.getByText(firstItem)
 
-        const itemCheckbox = TodoItem.locator('xpath=..').getByRole('checkbox')
-        const debugCheckbox = itemCheckbox.evaluate($i => $i.outerHTML)
-        console.log(debugCheckbox)
+        const itemCheckbox = TodoItem
+			.locator('xpath=..')
+			.getByRole('checkbox')
+			
         await debug.eachElement(itemCheckbox)
         await itemCheckbox.check()
         expect(itemCheckbox.isChecked).toBeTruthy()
