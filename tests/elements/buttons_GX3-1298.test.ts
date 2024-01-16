7adfc737537a347eff896b05c4ac541260425687
Belén <belleloweryart@gmail.com>
2024-01-16 11:40:13 -0300

    All test design finished

--- tests/elements/buttons_GX3-1298.test.ts
+++ tests/elements/buttons_GX3-1298.test.ts
@@ -1,18 +1,45 @@
-import { precondition, story, test, expect } from '@pages/TestBase';
+import { story, precondition, test, expect } from '@pages/TestBase';
 
 story('GX3-1298: Element Buttons', () => {
 	precondition(async ({ page }) => {
-		await page.goto('/buttons');
+		await page.goto('/buttons', { waitUntil: 'domcontentloaded' });
+	});
+	test('TC1: Should trigger when using double click', async ({ page }) => {
+		const messageExpected = 'You have done a double click';
+		const messageElement = page.locator('#doubleClickMessage');
 
-		test('TC1: Should trigger when using double click', async ({ page, chalPage }) => {
-			expect(1).toBe(1);
+		await test.step('Click Double Button', async () => {
+			await page.locator('#doubleClickBtn').dblclick();
 		});
-
-		test('TC2: Should trigger when using right click', async ({ page, chalPage }) => {
-			expect(1).toBe(1);
+		await test.step('Verify the Message', async () => {
+			await expect(messageElement).toHaveText(messageExpected);
 		});
+	});
 
-		test('TC3: Should trigger when using simple click', async ({ page, chalPage }) => {});
-		expect(1).toBe(1);
+	test('TC2: Should trigger when using right click', async ({ page }) => {
+		const messageExpected = 'You have done a right click';
+		const messageElement = page.locator('#rightClickMessage');
+
+		await test.step('Click Right Button', async () => {
+			await page.locator('#rightClickBtn').click({ button: 'right' });
+			await expect(messageElement).toHaveText('messageExpected');
+		});
+		await test.step('Verify the Message', async () => {
+			await expect(messageElement).toHaveText('messageExpected');
+		});
+	});
+
+	test('TC3: Should trigger when using simple click', async ({ page }) => {
+		const messageExpected = 'You have done a dynamic click';
+		const messageElement = page.locator('#2WTAN');
+		await page.getByText('Click Me', { exact: true }).click();
+
+		await test.step('Click Right Button', async () => {
+			await page.locator('#rightClickBtn').click();
+			await expect(messageElement).toHaveText('messageExpected');
+		});
+		await test.step('Verify the Message', async () => {
+			await expect(messageElement).toHaveText('messageExpected');
+		});
 	});
 });
