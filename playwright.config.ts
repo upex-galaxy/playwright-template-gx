1c74bec5a5dc2cfec35acb60ce8333ca2d0e31c5
Laysecaplz <124854171+Laysecaplz@users.noreply.github.com>
2024-01-23 01:02:47 -0300

    first commit button

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,12 +54,12 @@ export default defineConfig({
 			use: { ...devices['Desktop Chrome'] },
 		},
 
-		{
+		/*{
 			name: 'firefox',
 			use: { ...devices['Desktop Firefox'] },
-		},
+		//},
 		/* Test against branded browsers. */
-		{
+		/*{
 			name: 'edge',
 			use: { channel: 'msedge' },
 		},
