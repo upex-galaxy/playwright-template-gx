d0c3dfb899d78d76f608bfaf49ae0389653ebaaf
saiotest <elyermad@gmail.com>
2024-02-01 07:10:42 -0300

    fix: login credentials for spacebeyond and env vars for trello in regression

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	// Test Repo Directory:
 	testDir: './tests',
 	/* Maximum time one test can run for. */
-	timeout: 20 * 1000,
+	timeout: 40 * 1000,
 	expect: {
 		/**
 		 * Maximum time expect() should wait for the condition to be met.
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. Workers are kinda flaky! not prefer to use them */
-	workers: process.env.CI ? 1 : 1,
+	workers: process.env.CI ? 4 : 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: [
 		['./tests/custom-reporter.ts'],
